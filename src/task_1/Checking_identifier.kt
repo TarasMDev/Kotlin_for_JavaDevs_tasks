d2368468bcbe9fe538afedb711937852cb230f93
tarasm <taras-minenko@mail.ru>
2020-06-05 18:24:48 +0600

    Added second solution for the first problem

--- src/task_1/Checking_identifier.kt
+++ src/task_1/Checking_identifier.kt
@@ -5,15 +5,25 @@ package task_1
 
 //Since Kotlin_1.3 args can be omitted in main function
 fun main() {
-    println(isValidIdentifier("name"))   // true
-    println(isValidIdentifier("_name"))  // true
-    println(isValidIdentifier("_12"))    // true
-    println(isValidIdentifier(""))       // false
-    println(isValidIdentifier("012"))    // false
-    println(isValidIdentifier("no$"))    // false
+    println(isValidId("name"))   // true
+    println(isValidId("_name"))  // true
+    println(isValidId("_12"))    // true
+    println(isValidId(""))       // false
+    println(isValidId("012"))    // false
+    println(isValidId("no$"))    // false
 }
 
 //If it works, it works. Can be implemented in different ways
 fun isValidIdentifier(s: String): Boolean =
     !(s.isEmpty() || s in "0".."9" || s.contains("$"))
 
+//Second implementation
+fun isValidId(s: String): Boolean {
+    fun checkChar(c: Char) =
+        c == '_' || c.isLetterOrDigit()
+    if (s.isEmpty() || s[0].isDigit()) return false
+    for (i in s) {
+        if (!checkChar(i)) return false
+    }
+    return true
+}
